9fdd87f432963ad3c63fc8edaf8c10197ca19a8d
mphstar <bintang.simdig20@gmail.com>
2023-10-03 23:01:37 +0700

    add project

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: '*'
+    }
+});
+
+// Menyajikan halaman HTML sederhana
+app.get("/", (req, res) => {
+  res.json({
+    status: "Running",
+  });
+});
+
+// Mengelola koneksi socket.io
+io.on("connection", (socket) => {
+  console.log("connected");
+  socket.on("disconnect", function () {
+    console.log("disconnected");
+  });
+
+  socket.on('send_from_arduino', (data) => {
+    console.log(data);
+    io.emit('to_client', data)
+  })
+
+});
+
+
+const port = process.env.PORT || 4000;
+server.listen(port, () => {
+  console.log(`Server berjalan pada port ${port}`);
+});
